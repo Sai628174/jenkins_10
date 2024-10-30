edd672a99d2b721bfb9e5db885ddc19f2c159c50
AUTBLT410 <AUTBLT410@LT410-52.allianceblr.com>
2024-10-30 15:44:54 +0530

    This is my git java bush

---
+++ src/abc/firstpro.java
@@ -0,0 +1,12 @@
+package abc;
+
+public class firstpro {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to allaince - Jenkins");
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
